bbcb48667bd9b25af559df52bc9ad8a48ed4a78b
jillo-abdullahi <jillo.abdulla@gmail.com>
2023-05-13 12:43:47 +0300

    fix missing Tailwind purge

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -3,16 +3,28 @@ import Link from "next/link";
 
 export const Nav: React.FC = () => {
   const mobileAndTabletStyles = "w-full flex-row h-fit rounded-none top-0";
-  const desktopStyles = "md:left-0 md:w-fit md:flex-col md:h-full md:min-h-screen md:rounded-r-2.5xl";
+  const desktopStyles =
+    "md:left-0 md:w-fit md:flex-col md:h-full md:min-h-screen md:rounded-r-2.5xl";
   return (
     <nav
       className={`fixed bg-blue-700 flex items-center justify-between ${mobileAndTabletStyles} ${desktopStyles}`}
     >
-      <Link href="/">
-        <Image src="/images/icon-logo.svg" alt="logo" width={80} height={80} className="cursor-pointer" />
-      </Link>
+      <Image
+        src="/images/icon-logo.svg"
+        alt="logo"
+        width={80}
+        height={80}
+        className="cursor-pointer"
+      />
+
       <div className="flex items-center p-4">
-        <Image className="rounded-full" src="/images/icon-sample-avatar.jpeg" alt="avatar" width={40} height={40} />
+        <Image
+          className="rounded-full"
+          src="/images/icon-sample-avatar.jpeg"
+          alt="avatar"
+          width={40}
+          height={40}
+        />
       </div>
     </nav>
   );
